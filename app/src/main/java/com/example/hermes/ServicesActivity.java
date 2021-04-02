6171f3f5dbb262373317ba436777a5a22491e812
shalam15 <salamajayi@yahoo.com>
2021-04-02 12:43:25 +0100

    got messaging service list from firebase, I also added the default value for login, to speedup testing

--- app/src/main/java/com/example/hermes/ServicesActivity.java
+++ app/src/main/java/com/example/hermes/ServicesActivity.java
@@ -3,6 +3,7 @@ package com.example.hermes;
 import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -11,34 +12,60 @@ import android.widget.Toast;
 import android.view.View;
 
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class ServicesActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServicesActivity extends AppCompatActivity {
+String MessagingPlatform = "personal-messagingservices";
     // Array of strings...
     String[] mobileArray = {"Text","Email","Facebook","Instagram",
             "Twitter","WhatsApp","Discord","Slack"};
 
+    // Firebase Fields
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private FirebaseAuth mAuth;
+    private static String TAG = "FROM FIREBASE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.services_listview);
-
-        ArrayAdapter adapter = new ArrayAdapter<String>(this,
-                R.layout.activity_listview, mobileArray);
-
-        ListView listView = (ListView) findViewById(R.id.mobile_list);
-        listView.setAdapter(adapter);
-
-        // Listener function checking to see if any of the services are clicked on by user.
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        List<String> list = new ArrayList<>();
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+        //Get document names from collection
+        db.collection(MessagingPlatform).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                switchActivities2();
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        list.add(document.getString("platform"));
+                    }
+                } else {
+                    Log.i(TAG, "Error getting documents: ", task.getException());
+                }
+                Log.i("LOGGER", list.toString());
+                ArrayAdapter adapter = new ArrayAdapter<String>(ServicesActivity.this.getApplicationContext(), android.R.layout.simple_list_item_1,  list);
+                ListView listView = (ListView) findViewById(R.id.mobile_list);
+                listView.setAdapter(adapter);
+                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        switchActivities2();
+                    }
+                });
             }
         });
-
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.services_listview);
         // This is for the back button.
         View switchToSecondActivity = findViewById(R.id.back_button1);
         switchToSecondActivity.setOnClickListener(new View.OnClickListener() {
