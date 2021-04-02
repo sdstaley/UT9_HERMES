fffc71c23c05ab3f9cad75fadb6358fc726063a8
hermes <millionghebre1@gmail.com>
2021-04-01 20:16:12 -0400

    logo design and color schemes for app

--- app/src/main/java/com/example/hermes/LoginActivity.java
+++ app/src/main/java/com/example/hermes/LoginActivity.java
@@ -8,16 +8,25 @@ import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = "LoginActivity";
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_view);
 
+        // Initialize xml
         View editTextEmailAddress = findViewById(R.id.editTextEmailAddress);
         View editTextPassword = findViewById(R.id.editTextPassword);
         View buttonLogin = findViewById(R.id.buttonLogin);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
 
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
