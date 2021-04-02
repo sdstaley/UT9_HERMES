4003b35524fdb325ec2fe38a52e5200579850054
hermes <millionghebre1@gmail.com>
2021-04-01 21:28:54 -0400

    successful login with firebase login credentials + edge cases

--- app/src/main/java/com/example/hermes/LoginActivity.java
+++ app/src/main/java/com/example/hermes/LoginActivity.java
@@ -1,29 +1,44 @@
 package com.example.hermes;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
     private static final String TAG = "LoginActivity";
     private FirebaseAuth mAuth;
+    private EditText editTextEmailAddress, editTextPassword;
+    private Button buttonLogin;
+    private String email;
+    private String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_view);
 
-        // Initialize xml
-        View editTextEmailAddress = findViewById(R.id.editTextEmailAddress);
-        View editTextPassword = findViewById(R.id.editTextPassword);
-        View buttonLogin = findViewById(R.id.buttonLogin);
+        // Declare text fields and button
+        editTextEmailAddress = findViewById(R.id.editTextEmailAddress);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        buttonLogin = findViewById(R.id.buttonLogin);
 
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
@@ -31,9 +46,35 @@ public class LoginActivity extends AppCompatActivity {
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                switchToMainActivity();
+                email = editTextEmailAddress.getText().toString();
+                password = editTextPassword.getText().toString();
+                if(!email.equals("") && !password.equals("")) {
+                    loginMethod(email, password);
+                } else {
+                    Toast.makeText(LoginActivity.this, "You must enter your email and password to sign in.", Toast.LENGTH_SHORT).show();
+                }
             }
         });
+    }
+    
+    private void loginMethod(String email, String password) {
+        mAuth.signInWithEmailAndPassword(this.email, this.password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d(TAG, "signInWithEmail:success");
+                                    Toast.makeText(LoginActivity.this, "Successful login!", Toast.LENGTH_SHORT).show();
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    switchToMainActivity();
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.d(TAG, "signInWithEmail:failure", task.getException());
+                                    Toast.makeText(LoginActivity.this, "Login error with incorrect email address and/or password! Please try again.", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
     }
 
     private void switchToMainActivity() {
